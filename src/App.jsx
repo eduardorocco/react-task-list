ed30ecde67be16ead7acfce15b78407b7b890aad
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-26 17:39:50 +0100

    ADD current tasks

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,28 @@ const title = 'Task Manager'
 
 function App() {
 
+    const backlogOrInProgress = tasks.filter(task => 
+        task.state === 'backlog' ||
+        task.state === 'in_progress'
+    )
     return (
         <>
             <div className='header'>
                 <h1>{title}</h1>
             </div>
-
-            <h3>Current Tasks</h3>
+            <div className='container'>
+                <h2>Current Tasks</h2>
+                <ul>
+                    {backlogOrInProgress.map(task => (
+                        <li>
+                            <strong>{task.title}</strong>
+                            <p>Priority:{task.priority}</p>
+                            <p>Est. time:{task.estimatedTime}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
         </>
         
     )
