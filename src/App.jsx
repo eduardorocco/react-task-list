cb6fa98c7f970c886a08b9055d3b559e71064844
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-26 17:49:19 +0100

    ADD completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,29 @@ function App() {
         task.state === 'backlog' ||
         task.state === 'in_progress'
     )
+
+    const completed = tasks.filter(task => task.state === 'completed')
     return (
         <>
             <div className='header'>
                 <h1>{title}</h1>
             </div>
             <div className='container'>
-                <h2>Current Tasks</h2>
+                <h2>Current Tasks({backlogOrInProgress.length})</h2>
                 <ul>
                     {backlogOrInProgress.map(task => (
+                        <li>
+                            <strong>{task.title}</strong>
+                            <p>Priority:{task.priority}</p>
+                            <p>Est. time:{task.estimatedTime}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="container">
+                <h2>Completed Tasks({completed.length})</h2>
+                <ul>
+                {completed.map(task => (
                         <li>
                             <strong>{task.title}</strong>
                             <p>Priority:{task.priority}</p>
